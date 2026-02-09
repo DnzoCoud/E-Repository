c4dbe92e9c194a35bd0306affec89731ac8bdcf2
Daniel <dangelsfocus@gmail.com>
2026-02-09 13:17:15 -0500

    site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site:"https://DnzoCoud.github.io",
+  base: "/E-Repository/",
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
 });
